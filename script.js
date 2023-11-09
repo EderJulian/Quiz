33377c17587a65c78b1cd94d15ff4e89e777ce2b
Laura Garcia <garcialauracal@gmail.com>
2023-11-10 00:33:02 +0100

    maquetacion pagina questions

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const homeContainer = document.getElementById("home-container");
 const questionsContainer = document.getElementById("questions-container");
 const resultsContainer = document.getElementById("results-container");
-const startButton = document.getElementById('start-button')
+const startButton = document.getElementById("start-button");
 
 function goQuestion() {
-    homeContainer.classList.add("hide");
-    questionsContainer.classList.remove("hide");
+  homeContainer.classList.add("hide");
+  questionsContainer.classList.remove("hide");
 }
 
 startButton.addEventListener("click", goQuestion);
